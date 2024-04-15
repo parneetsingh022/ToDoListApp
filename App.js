25b77317a8e18ead0bb3db1f431ac1d49cfdd775
parneetsingh022 <parneetsingh022@gmail.com>
2024-04-14 19:21:29 -0700

    Added categories title

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AddCategory from './components/AddCategory';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Header />
       <AddCategory />
       <StatusBar style="auto" />
     </View>
